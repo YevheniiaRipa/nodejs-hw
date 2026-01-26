9687e2b476c01b784564fd00ac12e80e4c8c052f
Yevhiniia Ripa <evgeniarepa@gmail.com>
2026-01-26 23:57:30 +0200

    Fix bugs

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -15,17 +15,17 @@ export const getAllNotes = async (req, res) => {
     notesQuery.where('tag').equals(tag);
   }
 
-  const [totaNotes, notes] = await Promise.all([
+  const [totalNotes, notes] = await Promise.all([
     notesQuery.clone().countDocuments(),
     notesQuery.skip(skip).limit(perPage),
   ]);
 
-  const totalPages = Math.ceil(totaNotes / perPage);
+  const totalPages = Math.ceil(totalNotes / perPage);
 
   res.status(200).json({
     page,
     perPage,
-    totaNotes,
+    totalNotes,
     totalPages,
     notes,
   });
