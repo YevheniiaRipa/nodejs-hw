6d7efc68f009a059106000af8b70ab60880a9365
Yevhiniia Ripa <evgeniarepa@gmail.com>
2026-02-07 22:02:42 +0200

    Add password reset and user avatar upload

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 import cookieParser from 'cookie-parser';
 
@@ -25,6 +26,7 @@ app.use(cookieParser());
 
 app.use(notesRoutes);
 app.use(authRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
